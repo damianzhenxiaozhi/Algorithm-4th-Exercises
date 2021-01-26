84bc518e9fea8255a53e6d69d5ca2fd0801dd1fc
fanzhen <fanzhen@owinfo.net>
2021-01-26 14:17:33 +0800

    change package name

--- src/main/java/chapter1_1/E1_1_3.java
+++ src/main/java/chapter1_1/E1_1_3.java
@@ -1,20 +1,14 @@
 package chapter1_1;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class E1_1_3 {
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Enter at least 3 numbers");
-            return;
-        }
-
         int i = Integer.parseInt(args[0]);
         int j = Integer.parseInt(args[1]);
         int k = Integer.parseInt(args[2]);
 
-        if (i == j && j == k) {
-            System.out.println("equal");
-        } else {
-            System.out.println("not equal");
-        }
+        if (i == j && j == k) StdOut.println("equal");
+        else StdOut.println("not equal");
     }
 }
---
+++ src/main/java/chapter1_3/E1_3_1.java
@@ -0,0 +1,5 @@
+package chapter1_3;
+
+public class E1_3_1 {
+
+}
