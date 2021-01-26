e5f4b91fe0bb8b83ff31cf2615cf5c92920a0934
fanzhen <fanzhen@owinfo.net>
2021-01-26 14:26:19 +0800

    optimize code

--- src/main/java/chapter1_1/E1_1_5.java
+++ src/main/java/chapter1_1/E1_1_5.java
@@ -1,18 +1,19 @@
 package chapter1_1;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 
 public class E1_1_5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
-        double x = scanner.nextDouble();
-        double y = scanner.nextDouble();
-
-        if ((x > 0 && x < 1) && (y > 0 && y < 1)) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
+        while (!StdIn.isEmpty()) {
+            String[] nums = StdIn.readLine().split("\\s+");
+            double x = Double.parseDouble(nums[0]);
+            double y = Double.parseDouble(nums[1]);
+            StdOut.println(x > 0 && x < 1 && y > 0 && y < 1);
         }
     }
 }
